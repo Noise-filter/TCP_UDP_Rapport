cda8a93e4f8775811c30a0359883a03f5bf7f4ce
Pontus Fransson <pontus.fransson92@gmail.com>
2014-01-10 08:28:59 +0100

    Stuff

---
+++ TcpUdpRapport/TcpUdpRapport/ClientTCP.cpp
@@ -0,0 +1,26 @@
+#include "ClientTCP.h"
+
+ClientTCP::ClientTCP()
+{
+
+}
+
+ClientTCP::~ClientTCP()
+{
+
+}
+
+void ClientTCP::Connect(char ip[], unsigned short port)
+{
+
+}
+
+void ClientTCP::Send()
+{
+
+}
+
+void ClientTCP::Shutdown()
+{
+
+}
--- TcpUdpRapport/TcpUdpRapport/main.cpp
+++ TcpUdpRapport/TcpUdpRapport/main.cpp
@@ -1,8 +1,48 @@
 #include <iostream>
 using namespace std;
 
+#include "WinsockFunctions.h"
+
+#include "ClientTCP.h"
+#include "ClientUDP.h"
+#include "ServerTCP.h"
+
+void ClientUpdate();
+void ServerUpdate();
+
+bool RunServer = true;
+bool UDP = true;
+
+const char IP[] = "192.168.1.1";
+
+ClientTCP clientTCP;
+ClientUDP clientUDP;
+ServerTCP serverTCP;
+
 int main()
 {
+	InitWinSock();
+
+	if(RunServer)
+	{
+		ServerUpdate();
+	}
+	else
+	{
+		ClientUpdate();
+	}
+
+	ShutdownWinSock();
 
 	return 0;
+}
+
+void ClientUpdate()
+{
+
+}
+
+void ServerUpdate()
+{
+
 }
