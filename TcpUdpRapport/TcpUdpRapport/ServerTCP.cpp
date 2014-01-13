7f6ec467fd1322ee837ab783a4901ea06324c1c4
Pontus Fransson <pontus.fransson92@gmail.com>
2014-01-13 08:38:55 +0100

    sad

--- TcpUdpRapport/TcpUdpRapport/ServerTCP.cpp
+++ TcpUdpRapport/TcpUdpRapport/ServerTCP.cpp
@@ -1,2 +1,26 @@
 #include "ServerTCP.h"
 
+ServerTCP::ServerTCP()
+{
+
+}
+
+ServerTCP::~ServerTCP()
+{
+
+}
+
+void ServerTCP::Init(unsigned short port)
+{
+
+}
+
+int ServerTCP::Accept()
+{
+
+}
+
+void ServerTCP::Shutdown()
+{
+	
+}
